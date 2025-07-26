c2a45d943e61fa7e2c15e1325d221213caaca894
delano <delano@onetimesecret.com>
2025-07-25 21:04:30 -0700

    Convert tryouts to rspec

---
+++ spec/uri_redis_spec.rb
@@ -0,0 +1,67 @@
+# Generated rspec code for /Users/d/Projects/opensource/d/uri-redis/try/10_uri_redis_try.rb
+# Updated: 2025-07-25 21:01:08 -0700
+
+RSpec.describe '10_uri_redis_try' do
+  before(:all) do
+    require "uri/redis"
+  end
+
+  it 'Default database is 0' do
+    result = begin
+      uri = URI.parse "redis://localhost"
+      [uri.db, uri.host, uri.port]
+    end
+    expect(result).to eq([0, 'localhost', 6379])
+  end
+
+  it 'Can parse a redis URI with a database' do
+    result = begin
+      uri = URI.parse "redis://localhost/2"
+      [uri.db, uri.host, uri.port]
+    end
+    expect(result).to eq([2, 'localhost', 6379])
+  end
+
+  it 'Parsed URI can be accessed via conf hash' do
+    result = begin
+      uri = URI.parse "redis://localhost:16739/2"
+      [uri.scheme, uri.conf]
+    end
+    expect(result).to eq(['redis', {:host=>"localhost", :port=>16739, :db=>2, :ssl=>false}])
+  end
+
+  it 'Can parse a key name' do
+    result = begin
+      uri = URI.parse "redis://localhost/2/v1:arbitrary:key"
+      [uri.key, uri.db, uri.host, uri.port]
+    end
+    expect(result).to eq(['v1:arbitrary:key', 2, 'localhost', 6379])
+  end
+
+  it 'Can set db' do
+    result = begin
+      uri = URI.parse "redis://localhost/2/v1:arbitrary:key"
+      uri.db = 6
+      uri.to_s
+    end
+    expect(result).to eq('redis://localhost/6/v1:arbitrary:key')
+  end
+
+  it 'Can set key' do
+    result = begin
+      uri = URI.parse "redis://localhost/2/v1:arbitrary:key"
+      uri.key = "v2:arbitrary:key"
+      uri.to_s
+    end
+    expect(result).to eq('redis://localhost/2/v2:arbitrary:key')
+  end
+
+  it 'Support rediss' do
+    result = begin
+      uri = URI.parse "rediss://localhost"
+      [uri.scheme, uri.conf]
+    end
+    expect(result).to eq(["rediss", {:host=>"localhost", :port=>6379, :db=>0, :ssl=>true}])
+  end
+
+end
