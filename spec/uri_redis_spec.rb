cb11a637756132c5564ca8e46b05f11aa3b95724
delano <delano@onetimesecret.com>
2025-07-25 22:15:31 -0700

    Fix: Correct default Redis port

--- spec/uri_redis_spec.rb
+++ spec/uri_redis_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.describe URI::Redis do
+require_relative '../lib/uri/redis'
+
+RSpec.describe 'URI::Redis' do
   before(:all) do
     require "uri/redis"
   end
@@ -23,10 +25,10 @@ RSpec.describe URI::Redis do
 
   it 'Parsed URI can be accessed via conf hash' do
     result = begin
-      uri = URI.parse "redis://localhost:16_739/2"
+      uri = URI.parse "redis://localhost:6379/2"
       [uri.scheme, uri.conf]
     end
-    expect(result).to eq(['redis', { host: "localhost", port: 16_739, db: 2, ssl: false }])
+    expect(result).to eq(['redis', { host: "localhost", port: 6379, db: 2, ssl: false }])
   end
 
   it 'Can parse a key name' do
